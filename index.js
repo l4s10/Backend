4061c0260bf754ea93ec1cfff277beeeda59fd2e
francois <franciscomunoz142@gmail.com>
2022-11-13 15:27:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// app.listen(3000, () => console.log('server started'));
+
+app.listen(process.env.PORT, () =>{
+    console.log('server corriendo en el puerto -> ', process.env.PORT);
+})
+
+mongoose.set('useFindAndModify',false);
+mongoose.set('useNewUrlsParser',true);
+mongoose.set('useCreateIndex',true);
+mongoose.set('useUnifiedTopology',true);
+
+mongoose.connect(process.env.DB,(err) => {
+    if(err){
+        return console.log('Error al conectar con la base de datos');
+    }
+    return console.log('Conectado a la base de datos')
+})
