d47b9df5836a859ad49acbacb98cb7ae806ea2cf
Francisco <franciscomunoz142@gmail.com>
2022-11-13 20:43:33 -0300

    Solucion Bugs

--- controllers/mantentionController.js
+++ controllers/mantentionController.js
@@ -18,15 +18,15 @@ const createMantention = (req, res) =>{
 
 /*Funcionalidad Jorge*/
 const deleteMantention = (req, res) =>{
-    const {id} = req.body;
-    mantention.findByIdAndDelete(id, (err, mantention) =>{
+    const {id} = req.params;
+    mantention.findByIdAndDelete(id, (err, mantentions) =>{
         if(err){
             return res.status(400).send({message: "Error al eliminar la mantencion"})
         }
-        if(!mantention){
+        if(!mantentions){
             return res.status(404).send({message: "Error al eliminar la mantencion solicitada"})
         }
-        return res.status(200).send(mantention)
+        return res.status(200).send(mantentions)
     });
 }
 
