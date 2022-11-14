7f4e4cd3a9089ed6f4b3b8532179fa5925646a94
Francisco <franciscomunoz142@gmail.com>
2022-11-13 21:53:52 -0300

    add better comments

--- routes/mantentionRoutes.js
+++ routes/mantentionRoutes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const api = express.Router();
 const mantentionController = require('../controllers/mantentionController');
 
-//Enlace insomnia localhost:3001/api/create-mantention
+/* Enlaces insomnia http://localhost:(env.PORT)/api/...
+-> http://localhost:(env.PORT)/api/Mantention/Create : Crear mantencion
+-> http://localhost:(env.PORT)/api/Mantention/delete/(id) : Eliminar mantencion
+-> http://localhost:(env.PORT)/api/Mantentions/view : Ver mantenciones
+*/
 api.post('/Mantention/Create', mantentionController.createMantention);
 api.delete('/Mantention/delete/:id', mantentionController.deleteMantention);
 api.get('/Mantentions/view', mantentionController.getMantention);
